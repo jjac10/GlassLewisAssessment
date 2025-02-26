568144f0191108def894879a03850c0cb2f627d8
Jose Joaquín Alarcón <Jose.Alarcon@madic-uk.com>
2025-02-26 17:35:08 +0100

    Domain layer ended

---
+++ GlassLewisAssessment.domain/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlassLewisAssessment.domain.Entities;
+
+namespace GlassLewisAssessment.domain.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        Task<List<Company>> GetAllAsync();
+        Task<Company?> GetByIdAsync(int id);
+        Task<Company?> GetByIsinAsync(string isin);
+        Task<int> CreateAsync(Company company);
+        Task<int> UpdateAsync(Company company);
+    }
+}
