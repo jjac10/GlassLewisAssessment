8642944ebc9d120da880e2d44d4415adfeb715d3
Jose Joaquín Alarcón <Jose.Alarcon@madic-uk.com>
2025-02-26 16:35:43 +0100

    Created entity Company

---
+++ GlassLewisAssessment.domain/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlassLewisAssessment.domain.Entities
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string Exchange { get; set; }
+        [Required]
+        public required string Ticker { get; set; }
+        [Required]
+        public required string Isin { get; set; }
+        public string? Website { get; set; }
+    }
+}
